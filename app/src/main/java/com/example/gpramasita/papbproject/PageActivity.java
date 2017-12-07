fd8d49355e5ef1932cd2f61f5e01bb0ace7b0963
Prams <pgustiarum@gmail.com>
2017-12-07 21:48:57 +0700

    Version 1.0.0
    Tambahan Layout Fragment

---
+++ app/src/main/java/com/example/gpramasita/papbproject/PageActivity.java
@@ -0,0 +1,45 @@
+package com.example.gpramasita.papbproject;
+
+import android.graphics.pdf.PdfDocument;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by gpramasita on 12/7/17.
+ */
+
+public class PageActivity extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public PageActivity (FragmentManager manage, int tabCount){
+        super(manage);
+        this.tabCount = tabCount;
+    }
+
+    @Override
+    public Fragment getItem(int position){
+        switch (position){
+            case 0:
+                FragmentAccount fragmentAccount = new FragmentAccount();
+                return fragmentAccount;
+            case 1:
+                FragmentDetails fragmentDetails = new FragmentDetails();
+                return fragmentDetails;
+            case 2:
+                FragmentSocial fragmentSocial = new FragmentSocial();
+                return fragmentSocial;
+            case 3:
+                FragmentLeader fragmentLeader = new FragmentLeader();
+                return fragmentLeader;
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount(){
+        return tabCount;
+    }
+
+}
